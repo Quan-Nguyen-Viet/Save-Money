9a8599e0663f1b58181aadc7cac847fde862192d
vQuan <17110210@student.hcmute.edu.vn>
2021-11-22 21:53:20 +0700

    Update savingCtrl.js

--- backend/controllers/savingCtrl.js
+++ backend/controllers/savingCtrl.js
@@ -3,8 +3,20 @@ const Customers = require('../models/customerModel')
 
 const savingCtrl = {
   getSavingAccount: async (req, res) =>{},
+  
   createSavingAccount: async(req, res) =>{},
+  
   deleteSavingAccount: async(req, res) =>{},
+
+  getNonTermIR: async(req, res) =>{
+    try {
+      const userID = req.Customers._id
+    } catch (err) {
+      console.error(err)
+    }
+  },
+  getNonTermIR: async(req, res) =>{}
+  /*
   getNonTermIR: async(id, balance, interestRate) =>{
     await Customers.findByIdAndUpdate({_id: id},{
       getNonTermIR: (balance*(interestRate/360))
@@ -14,7 +26,7 @@ const savingCtrl = {
     await Customers.findByIdAndUpdate({_id: id},{
       get3MTermIR: (balance*(interestRate/12)*3)
     })
-  }
+  }*/
 }
 
 module.exports = savingCtrl

add50e969362a1c59407b395b8cf1c213c23eb80
vQuan <17110210@student.hcmute.edu.vn>
2021-11-22 18:31:16 +0700

    add non-term and 3M term

--- backend/controllers/savingCtrl.js
+++ backend/controllers/savingCtrl.js
@@ -1,8 +1,21 @@
 const Savings = require('../models/savingModel')
 const Customers = require('../models/customerModel')
 
-const growthInterest = async (id, balance, interestRate) => {
-  await Customers.findByIdAndUpdate({_id: id},{
-    
-  })
+const savingCtrl = {
+  getSavingAccount: async (req, res) =>{},
+  createSavingAccount: async(req, res) =>{},
+  deleteSavingAccount: async(req, res) =>{},
+  getNonTermIR: async(id, balance, interestRate) =>{
+    await Customers.findByIdAndUpdate({_id: id},{
+      getNonTermIR: (balance*(interestRate/360))
+    })
+  },
+  get3MTermIR: async(id, balance, interestRate) =>{
+    await Customers.findByIdAndUpdate({_id: id},{
+      get3MTermIR: (balance*(interestRate/12)*3)
+    })
+  }
 }
+
+module.exports = savingCtrl
+

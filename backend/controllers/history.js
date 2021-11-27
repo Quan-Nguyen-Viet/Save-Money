521d7266f0bd095bf3f7300a719196907523b66e
vQuan <17110210@student.hcmute.edu.vn>
2021-11-27 19:32:15 +0700

    Create history.js
    add getHistory
    add createHistory

---
+++ backend/controllers/history.js
@@ -0,0 +1,23 @@
+import { find } from "lodash"
+import { HistoryModel } from "../models/histotyModel"
+
+export const getHistory = async (req, res) => {
+  try{
+      const history = await HistoryModel.find();
+      console.log('history', history);
+      res.status(200).json(history);
+  }catch (err){
+      res.status(500).json({ error: err});
+  }
+}
+
+export const createHistory = async (req, res) => {
+  try {
+      const newHistory = req.body;
+      const history = new HistoryModel(newHistory);
+      await history.save();
+      res.status(200).json(history);
+  } catch (err) {
+      res.status(500).json({ error: err});
+  }
+}

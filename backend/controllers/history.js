50ad34d9e3f89492a440b0ccadf13d08d8ba6b12
vQuan <17110210@student.hcmute.edu.vn>
2021-12-08 00:51:56 +0700

    add getHistoryById

--- backend/controllers/history.js
+++ backend/controllers/history.js
@@ -20,3 +20,14 @@ export const createHistory = async (req, res) => {
       res.status(500).json({ error: err});
   }
 }
+
+export const getHistoryById = async (req, res) => {
+  try{
+      const { userid } = req.body;
+      const history = await HistoryModel.findById(userid);
+      if(!user) return res.status(400).json({msg: "User history not exist."})
+      res.status(200).json(history);
+  }catch (err){
+      res.status(500).json({ error: err});
+  }
+}
